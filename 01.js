cb914a87937b3dd8d8e1933ab53c0dbb31321eb3
Nitesh <niteshgupta919843@gmail.com>
2026-04-13 20:08:20 +0530

    Add JS coding assignment solutions (Q1–Q10)

---
+++ 01.js
@@ -0,0 +1,82 @@
+// let arr = [10 ,20 , 30, 40, 50];
+// for(let i =0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+
+// function getArrayLenght(arr){
+//     return arr.length;
+// }
+
+// let nums = [10 ,20, 30, 40, 50];
+// console.log(getArrayLenght(nums))
+
+
+// function  getLastElement(Array){
+//     return Array.at(-1);
+// }
+// let nums = [10 ,20,30,40,50]
+// console.log(getLastElement(nums));
+
+// let  cars = ["Gwagon" , "BMW" , "Defender" , "Thar"];
+// cars.pop();
+// cars.shift();
+// console.log(cars)
+
+
+// let  cars = ["Gwagon" , "BMW" , "Defender" , "Thar"];
+// let   result = cars.indexOf("Thar");
+// console.log(result);
+
+
+// let arr = [10 ,20, 30, 40, 50];
+// for(let i = arr.length-1; i>=0; i--){
+//     console.log(arr[i])
+// }
+
+// function reverseArray(arr){
+//     let result  = [];
+//     for(let i =arr.length-1; i>=0; i--){
+//        result.push(arr[i]);
+//     }
+//     return result;
+// }
+// console.log(reverseArray([10,20,30,40]))
+
+
+// function mergeTwoArray(arr1 , arr2){
+  
+//        return [...arr1 , ...arr2];
+// }
+
+// console.log(mergeTwoArray ([10,20] , [30,40]));
+
+
+// function mergeArrays(...arrays){
+//     return [].concat(...arrays);
+// }
+
+// console.log(mergeArrays([10] , [20] , [30,40]))
+
+
+// function findLArgestNum(arr){
+
+
+// let max = [0];
+// for(let i =0; i<arr.length; i++){
+//      if(arr[i]> max){
+//         max = arr[i];
+//      }
+//     }
+//      return max;
+// }
+// let arr = [10,20,30,50, 40,15]
+// console.log(findLArgestNum(arr));
+
+
+// function removeDuplicate(arr){
+//     return [...new Set(arr)];
+// }
+// console.log(removeDuplicate([10,20,30,40,10,20]))
+
+
